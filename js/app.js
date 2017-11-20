0cf6edf05b579a283ccd127a6101305cf1a9afe0
Alejandro Gonzalez <alejandrogonzal-@outlook.com>
2017-11-19 19:49:52 -0500

    Updated

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('app', ['ui.router', 'home']);
+
+app.config(['$stateProvider','$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+  $stateProvider.state('app', {
+    abstract: true,
+    url: '',
+    template: '<ui-view></ui-view>'
+  }).state('app.home', {
+    url: '/home',
+    controller: 'home-controller as homeCtrl',
+    templateUrl: 'views/home.tpl.html'
+  })
+
+  $urlRouterProvider.otherwise('/home');
+}])
