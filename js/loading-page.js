6fab49910fc33627cfd81f61f90142c6b14536fa
Alejandro Gonzalez <alejandrogonzal-@outlook.com>
2017-11-25 19:32:08 -0500

    Updated

--- js/loading-page.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    var loading_container_aux = document.getElementById('loading_container');
-    loading_container_aux.style.visibility = 'hidden';
-    loading_container_aux.style.opacity = '0';    
-}
--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,9 @@
+window.onload = function(){
+    var loading_container_aux = document.getElementById('loading_container');
+    loading_container_aux.style.visibility = 'hidden';
+    loading_container_aux.style.opacity = '0';    
+}
+
 $(function(){
     var header = document.getElementById('header');
     var headroom = new Headroom(header);
@@ -35,3 +41,18 @@ $(function(){
             }
         });
 });
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    const slide = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slide.length; i++) {
+       slide[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > slide.length) {myIndex = 1}    
+    slide[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 1000); 
+}
